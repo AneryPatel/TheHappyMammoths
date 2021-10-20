03d1cc7cc67774eef50e616e9ee99191666c0d87
amcheyre-nw <91029949+amcheyre-nw@users.noreply.github.com>
2021-10-20 13:23:24 -0500

    Add files via upload

--- Checkpoint 2/src/Unsustained_percapita_table.sql
+++ Checkpoint 2/src/Unsustained_percapita_table.sql
@@ -1,10 +1,21 @@
-create table heatmap1 as
+create table heatmap3 as
     SELECT
-        data_officer.unsustained_count, data_area.polygon, data_area.median_income,
-        (case when sum(data_racepopulation.count)>0 then sum(data_officer.unsustained_count)/sum(data_racepopulation.count)::float end) unsustained_percapita
+        data_area.polygon, data_area.median_income, data_area.name,
+        (case when sum(data_racepopulation.count)>0 then sum(complaintcount)/sum(data_racepopulation.count)::float end) unsustained_percapita
     FROM data_officer
     JOIN data_area
       ON data_area.id = data_officer.id
     JOIN data_racepopulation
       ON data_racepopulation.id = data_area.id
-    group by polygon, unsustained_count, median_income;
+    JOIN (SELECT area_id, count(data_officerallegation.allegation_id) as complaintcount
+    FROM data_officerallegation
+    JOIN (SELECT d1.allegation_id, d1.area_id
+    FROM data_allegation_areas AS d1
+    JOIN data_allegation AS d2
+    ON d2.crid = d1.allegation_id
+    where d2.is_officer_complaint = 'FALSE') civilian_complaints
+    ON civilian_complaints.allegation_id = data_officerallegation.allegation_id
+    WHERE final_finding = 'UN' OR final_finding = 'EX' OR final_finding = 'NS'
+    GROUP BY area_id) complaints
+    ON data_racepopulation.area_id = complaints.area_id
+    group by polygon, median_income, name;
