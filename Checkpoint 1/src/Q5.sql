8e1b04472ab7dadff033a033cb631cc114cfeeb8
amcheyre-nw <91029949+amcheyre-nw@users.noreply.github.com>
2021-10-30 10:50:19 -0500

    Update Q5.sql

--- Checkpoint 1/src/Q5.sql
+++ Checkpoint 1/src/Q5.sql
@@ -2,6 +2,10 @@ SELECT
     SUM(CASE WHEN final_finding<>'SU' and is_officer_complaint = false THEN 1 ELSE 0 END):: float/SUM(CASE WHEN is_officer_complaint= true
     or is_officer_complaint= false THEN 1 ELSE 0 END)  percent_unsustained_civilian,
     SUM(CASE WHEN final_finding<>'SU' and is_officer_complaint = true THEN 1 ELSE 0 END):: float/SUM(CASE WHEN is_officer_complaint= true
-    or is_officer_complaint= false THEN 1 ELSE 0 END)  percent_unsustained_officer
+    or is_officer_complaint= false THEN 1 ELSE 0 END)  percent_unsustained_officer,
+    SUM(CASE WHEN final_finding='SU' and is_officer_complaint = false THEN 1 ELSE 0 END):: float/SUM(CASE WHEN is_officer_complaint= true
+    or is_officer_complaint= false THEN 1 ELSE 0 END)  percent_sustained_civilian,
+    SUM(CASE WHEN final_finding='SU' and is_officer_complaint = true THEN 1 ELSE 0 END):: float/SUM(CASE WHEN is_officer_complaint= true
+    or is_officer_complaint= false THEN 1 ELSE 0 END)  percent_sustained_officer
 
 FROM data_allegation JOIN data_officerallegation d on data_allegation.crid = d.allegation_id;
