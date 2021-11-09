8334ea7b5f12f8a9a0d82a021c605c77a70a809d
amcheyre-nw <91029949+amcheyre-nw@users.noreply.github.com>
2021-11-09 15:13:19 -0600

    Delete data_area_query.sql

--- Checkpoint 3/src/data_area_query.sql
+++
@@ -1,5 +0,0 @@
-SELECT data_area.name, data_area.area_type,
-           sum(data_officer.unsustained_count) as unsustained_allegations_sum, EXTRACT(YEAR from data_officer.appointed_date) as appointed_year
-    FROM data_officer, data_area
-    WHERE data_officer.id = data_area.id
-    GROUP BY data_area.name, data_area.area_type,data_officer.appointed_date;
